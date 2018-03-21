8bf3811185b9b3a9ff1e3385f0f955c091653580
George Cummins <github@8people.com>
2018-03-21 15:52:11 -0500

    Added the Message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [
     HeroService,
+    MessageService,
   ],
   bootstrap: [AppComponent]
 })
